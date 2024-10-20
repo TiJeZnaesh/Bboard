ba3aa527160b58dee587774a8543c2b197e4cedd
GordienkoNadya <noyabrka1983@gmail.com>
2024-10-20 14:13:13 +0300

    add login, about, logout

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, other_page, BBLoginView, BBLogoutView, profile
 
 app_name = 'main'
 urlpatterns = [
+    path("accounts/login/", BBLoginView.as_view(), name="login"),
+    path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
+    path("accounts/profile/", profile, name="profile"),
+    path("accounts/logout/", BBLogoutView.as_view(), name="logout")
 ]
