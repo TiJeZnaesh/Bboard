ed834bd66b32f29eeedb83741ca28635f655c83d
GordienkoNadya <noyabrka1983@gmail.com>
2024-11-03 14:27:44 +0300

    added all process registration

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView, BBLogoutView, profile, ProfileEditView
+from .views import index, other_page, BBLoginView, BBLogoutView, profile, ProfileEditView, PasswordEditView, RegisterView, RegisterDoneView, user_activate
+
 app_name = 'main'
 urlpatterns = [
     path("accounts/login/", BBLoginView.as_view(), name="login"),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
-    path("accounts/profile/edit/", ProfileEditView.as_view(), name = "profile_edit"),
+    path('accounts/password/edit/', PasswordEditView.as_view(), name='password_edit'),
+    path("accounts/profile/edit/", ProfileEditView.as_view(), name="profile_edit"),
     path("accounts/profile/", profile, name="profile"),
-    path("accounts/logout/", BBLogoutView.as_view(), name="logout")
+    path("accounts/logout/", BBLogoutView.as_view(), name="logout"),
+    path('accounts/activate/<str:sign>/', user_activate, name='activate'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterView.as_view(), name='register'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
-from  .models import AdvUser
-from  .forms import ProfileEditForm
+from .models import AdvUser
+from .forms import ProfileEditForm, RegisterForm
+from django.views.generic.base import TemplateView
+from django.core.signing import BadSignature
+from .utilities import signer
+
 
 def index(request):
     return render(request, 'main/index.html')
@@ -23,13 +27,21 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
+
 class BBLoginView(LoginView):
     template_name = "main/login.html"
+
+
 @login_required
 def profile(request):
     return render(request, "main/profile.html")
+
+
 class BBLogoutView(LogoutView):
     pass
+
+
 class ProfileEditView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     model = AdvUser
     template_name = "main/profile_edit.html"
@@ -40,8 +52,41 @@ class ProfileEditView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     def setup(self, request, *args, **kwargs):
         self.user_id = request.user.pk
         return super().setup(request)
+
     def get_object(self, queryset=None):
         if not queryset:
             queryset = self.get_queryset()
         return get_object_or_404(queryset, pk=self.user_id)
 
+
+class PasswordEditView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_edit.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя изменен'
+
+
+class RegisterView(CreateView):
+    model = AdvUser
+    template_name = 'main/register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('main:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'main/register_done.html'
+
+
+def user_activate(reguest, sign):
+    try:
+        username = signer.unsign(sign)
+    except BadSignature:
+        return render(reguest, "main/activation_failed.html")
+    user = get_object_or_404(AdvUser, username=username)
+    if user.is_activated:
+        template = "main/activation_done_earlier.html"
+    else:
+        template = "main/activation_done.html"
+        user.is_active = True
+        user.is_activated = True
+        user.save()
+    return render(reguest, template)
